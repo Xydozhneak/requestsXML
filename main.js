9de680874931bb54dcbef8d8f07e92c2b2439476
Bondarenko Vitalii <xydozhnikbomj@gmail.com>
2023-07-18 12:59:03 +0300

    1.1

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 const parse = (children) => JSON.parse(children);
 
+const xml = new XMLHttpRequest();
 const xml1 = new XMLHttpRequest();
-const xml2 = new XMLHttpRequest();
 
 
 
-xml1.open('GET', 'request/data.json');
-xml1.addEventListener('readystatechange', () => {
-  if (xml1.readyState === 4 && xml1.status < 400) {
-   const data = parse(xml1.responseText);
+xml.open('GET', 'request/data.json');
+xml.addEventListener('readystatechange', () => {
+  if (xml.readyState === 4 && xml.status < 400) {
+   const data = parse(xml.responseText);
 
-    xml2.open('GET', 'request/data1.json');
-    xml2.addEventListener('readystatechange', () => {
-      if (xml2.readyState === 4 && xml2.status < 400) {
-      const  data2 = parse(xml2.responseText);
+    xml1.open('GET', 'request/data1.json');
+    xml1.addEventListener('readystatechange', () => {
+      if (xml1.readyState === 4 && xml1.status < 400) {
+      const  data1 = parse(xml1.responseText);
 
-        const mergedArray = mergeArr(data.children, data2.children);
+        const mergedArray = mergeArr(data.children, data1.children);
         console.log(mergedArray);
       }
     });
-    xml2.send();
+    xml1.send();
   }
 });
-xml1.send();
+xml.send();
 
 const mergeArr = (arr1, arr2) => [...arr1, ...arr2];
